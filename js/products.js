ae09231e407dfcf6654f41e8614f4371a7c1289c
OlaOluwalekan <olalekanbello534@gmail.com>
2022-06-15 18:42:23 +0100

    product list items increased with random items

--- js/products.js
+++ js/products.js
@@ -38,6 +38,31 @@ let Product5 = new Product(
   "./images/products/5.jpg"
 );
 
+// let rndNo = Math.random() * 5;
+// let rndNoUp = Math.ceil(rndNo);
+
 const productList = [Product1, Product2, Product3, Product4, Product5];
+for (let i = 0; i < 20; i++) {
+  let roundNo = Math.random() * 5;
+  let randomNo = Math.ceil(roundNo);
+  console.log(randomNo);
+  switch (randomNo) {
+    case 1:
+      productList.push(Product1);
+      break;
+    case 2:
+      productList.push(Product2);
+      break;
+    case 3:
+      productList.push(Product3);
+      break;
+    case 4:
+      productList.push(Product4);
+      break;
+    case 5:
+      productList.push(Product5);
+      break;
+  }
+}
 
 export { productList };
