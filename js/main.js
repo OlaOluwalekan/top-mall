6c12d593e01082a4d9b19eabb1ef3572164fc20d
OlaOluwalekan <olalekanbello534@gmail.com>
2022-06-15 18:08:58 +0100

    product list adjusted with DOM dynamic element

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
+import { productList } from "./products.js";
 let contentHeadImageDiv = document.getElementById("contentImage");
+let productListDiv = document.querySelector(".product-list");
 
 let contentHeadImage = [
   "./images/content-head-images/girl-colourful.jpg",
@@ -19,6 +21,72 @@ let ChangeImage = () => {
   }
 };
 
+let AddToProductList = () => {
+  // let productContainer;
+  for (let i = 0; i < productList.length; i++) {
+    let productContainer = document.createElement("div");
+    let productImageDiv = document.createElement("div");
+    let productDetails = document.createElement("div");
+    productImageDiv.classList.add("product-image");
+    productDetails.classList.add("product-details");
+
+    // CREATE AND ADD PRODUCT IMAGE
+    let productImage = document.createElement("img");
+    productImage.src = productList[i].imageURL;
+    productImageDiv.appendChild(productImage);
+
+    // CREATE AND ADD PRODUCT NAME
+    let productName = document.createElement("h4");
+    let productNameText = document.createTextNode(productList[i].name);
+    productName.appendChild(productNameText);
+    productName.classList.add("product-name");
+
+    // CREATE AND ADD PRODUCT CATEGORY
+    let productCategory = document.createElement("p");
+    let productCategoryText = document.createTextNode(productList[i].category);
+    productCategory.appendChild(productCategoryText);
+    productCategory.classList.add("product-category");
+
+    // CREATE AND ADD PRODUCT PRICE
+    let productPrice = document.createElement("h3");
+    let productPriceText = document.createTextNode(productList[i].price);
+    productPrice.appendChild(productPriceText);
+    productPrice.classList.add("product-price");
+
+    // CREATE AND ADD PRODUCT ACTIONS TO LIKE AND ADD TO CART
+    let productActionsDiv = document.createElement("div");
+    let productAddToCartDiv = document.createElement("div");
+    let productLikeDiv = document.createElement("div");
+    productAddToCartDiv.classList.add("add-to-cart");
+    productLikeDiv.classList.add("like-product");
+    productActionsDiv.classList.add("product-actions");
+
+    let productAddToCart = document.createElement("i");
+    productAddToCartDiv.appendChild(productAddToCart);
+    productAddToCart.classList.add("fas", "fa-cart-plus");
+
+    let productLike = document.createElement("i");
+    productLikeDiv.appendChild(productLike);
+    productLike.classList.add("fas", "fa-heart");
+
+    productActionsDiv.appendChild(productAddToCartDiv);
+    productActionsDiv.appendChild(productLikeDiv);
+
+    productDetails.appendChild(productName);
+    productDetails.appendChild(productCategory);
+    productDetails.appendChild(productPrice);
+    productDetails.appendChild(productActionsDiv);
+
+    productContainer.appendChild(productImageDiv);
+    productContainer.appendChild(productDetails);
+    productContainer.classList.add("product-container");
+
+    productListDiv.appendChild(productContainer);
+    // console.log(productName);
+  }
+};
+
 window.onload = () => {
   ChangeImage();
+  AddToProductList();
 };
--- js/products.js
+++ js/products.js
@@ -11,7 +11,33 @@ let Product1 = new Product(
   "Laptop X-H350",
   "computing home-and-office",
   "245,000",
-  "./images/product/1.jpg"
+  "./images/products/1.jpg"
+);
+let Product2 = new Product(
+  "Cando Desktop JSC",
+  "computing home-and-office",
+  "133,000",
+  "./images/products/2.jpg"
+);
+let Product3 = new Product(
+  "Baby Feed",
+  "baby product",
+  "56,400",
+  "./images/products/3.jpg"
+);
+let Product4 = new Product(
+  "Diapers 250D",
+  "baby product",
+  "14,200",
+  "./images/products/4.jpg"
+);
+let Product5 = new Product(
+  "Smart 750X",
+  "computing home-and-office",
+  "78,990",
+  "./images/products/5.jpg"
 );
 
-const computingProducts = {};
+const productList = [Product1, Product2, Product3, Product4, Product5];
+
+export { productList };
